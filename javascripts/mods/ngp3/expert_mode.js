8ca7e0e1cd4f351a3fee18850c975f48fa9ccb77
Mark Li <37037391+ItMarki@users.noreply.github.com>
2020-11-11 23:36:59 +0800

    Update expert_mode.js

--- javascripts/mods/ngp3/expert_mode.js
+++ javascripts/mods/ngp3/expert_mode.js
@@ -5,6 +5,6 @@ function checkForExpertMode() {
 	metaSave.ngp3ex = true
 	if (document.getElementById("welcome").style.display != "flex") document.getElementById("welcome").style.display = "flex"
 	else player.aarexModifications.popUpId = ""
-	document.getElementById("welcomeMessage").innerHTML = "解鎖大撕裂的技能的時候，一片空間晶片跟現實碰撞，創造了一個平行宇宙，裡面所有東西都變得困難。解鎖了 NG+4 之外，你也解鎖了 NG+3 專家模式！請在加載選項裡的模組標籤裡查看。（注意：這個模組目前未完成）"
+	document.getElementById("welcomeMessage").innerHTML = "解鎖大撕裂的技能的時候，一片空間水晶跟現實碰撞，創造了一個平行宇宙，裡面的所有東西都變得困難。解鎖了 NG+4 之外，你也解鎖了 NG+3 專家模式！請在加載選項裡的模組標籤裡查看。（注意：這個模組目前未完成）"
 	localStorage.setItem(metaSaveId,btoa(JSON.stringify(metaSave)))
 }
