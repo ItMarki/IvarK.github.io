4ea0876f32885f3b27c0b43291194a64b9a94b44
Mark Li <37037391+ItMarki@users.noreply.github.com>
2020-11-11 23:36:35 +0800

    Update expert_mode.js

--- javascripts/mods/ngp3/expert_mode.js
+++ javascripts/mods/ngp3/expert_mode.js
@@ -1,10 +1,10 @@
 function checkForExpertMode() {
 	if (!metaSave.ngp4) return
 	if (metaSave.ngp3ex) return
-	console.log("EXPERT MODE UNLOCKED!")
+	console.log("已解鎖專家模式！")
 	metaSave.ngp3ex = true
 	if (document.getElementById("welcome").style.display != "flex") document.getElementById("welcome").style.display = "flex"
 	else player.aarexModifications.popUpId = ""
-	document.getElementById("welcomeMessage").innerHTML = "As you unlock the capability to Big Rip the universe, a space crystal collides with reality and creates an alternate universe where everything is more difficult. In addition to unlocking NG+4, you unlocked NG+3 Expert Mode! Check it in the Mods tab of your load options. (Note: Currently, this mod is unfinished)"
+	document.getElementById("welcomeMessage").innerHTML = "解鎖大撕裂的技能的時候，一片空間晶片跟現實碰撞，創造了一個平行宇宙，裡面所有東西都變得困難。解鎖 NG+4 之外，你也解鎖了 NG+3 專家模式！請在加載選項裡的模組標籤裡查看。（注意：這個模組目前未完成）"
 	localStorage.setItem(metaSaveId,btoa(JSON.stringify(metaSave)))
 }
